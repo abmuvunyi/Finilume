5aa6f0af2b4b380fb40b184aa91d6da012c11f9d
abmuvunyi <ab.muvunyi@gmail.com>
2025-08-12 23:10:20 +0200

    migration fix

--- db/migrate/20250812042841_add_status_to_feedbacks.rb
+++ db/migrate/20250812042841_add_status_to_feedbacks.rb
@@ -1,6 +1,16 @@
-class AddStatusToFeedbacks < ActiveRecord::Migration[7.2]
-  def change
-    add_column :feedbacks, :status, :string, null: false, default: "open"
+class AddStatusToFeedbacks < ActiveRecord::Migration[7.1]
+  def up
+    unless column_exists?(:feedbacks, :status)
+      add_column :feedbacks, :status, :string, null: false, default: "open"
+    else
+      # Make sure the constraints are what we expect
+      change_column_default :feedbacks, :status, "open"
+      change_column_null :feedbacks, :status, false
+    end
+  end
+
+  def down
+    # Only remove if present (keeps rollback safe)
+    remove_column :feedbacks, :status if column_exists?(:feedbacks, :status)
   end
 end
-
